f4675cb7629c57a353757db30dcd8949b0547195
Ajibson <azeezajibson123@gmail.com>
2021-08-18 22:12:42 +0100

    ...

--- user/urls.py
+++ user/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import signup,edit_resume
 urlpatterns = [
-    path('signup/', signup, name = "signup"),
+    path('', signup, name = "signup"),
     path('edit_resume/', edit_resume, name = "edit_resume")
 ]
